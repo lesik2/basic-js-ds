c19702c44b428a522c1d9ab8717c11a360c2b9cc
Aliaksei Pukhalski <lesikpuhalskij@gmail.com>
2023-04-13 23:27:46 +0300

    feat: solve queue.js.

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+ const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,23 +13,49 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
+
 class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(){
+    this.list=new ListNode();
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.list;
+  }
+
+  enqueue(value) {
+    if(this.list.value===undefined)
+    {
+      this.list.value=value;
+    }
+    else{
+      let currentNode=this.list;
+      let new_node=new ListNode();
+      new_node.value=value;
+     
+      while(currentNode.next!==null) currentNode=currentNode.next;
+      currentNode.next=new_node;
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(this.list.value!==undefined)
+    { let currentNode=this.list;
+      this.list=this.list.next;
+      return currentNode.value;
+    }
   }
 }
+const queue = new Queue();
+ queue.enqueue(1); // adds the element to the queue
+queue.enqueue(3); // adds the element to the queue
+ queue.dequeue(); // returns the top element from queue and deletes it, returns 1
+ console.log(queue.getUnderlyingList());
+
+
+
 
 module.exports = {
   Queue
